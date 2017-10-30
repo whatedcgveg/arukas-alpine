3eb0e695ab07eae3f499e04de0cae544ecd3c1ff
Lichard Huiman <32113947+whatedcgveg@users.noreply.github.com>
2017-10-30 18:31:43 +0800

    Update keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -16,6 +16,6 @@ if [ ! -f "/etc/ssh/ssh_host_ed25519_key" ]; then
   ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 > /dev/null
 fi
 
-mkdir 700 /root/.ssh
+mkdir -m 700 /root/.ssh
 echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
